9ac6635189610de16a2a394853f79c1847d08e39
Joannis Orlandos <joannis@orlandos.nl>
2017-03-24 13:47:56 +0100

    Exposing methods on vapor has been a little more worked out. No calls yet

--- Templates/MeowVapor.js
+++ Templates/MeowVapor.js
@@ -4,14 +4,36 @@ import MeowVapor
 import Vapor
 
 <%
-    // helper
+    // helpers
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    Object.size = function(obj) {
+        var size = 0, key;
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) size++;
+        }
+        return size;
+    };
+
+    function plural(name) {
+        let lastLetter = name.slice(-1);
+        name = name.substring(0, name.length - 1);
+
+        if(lastLetter == "y") {
+            return name + "ies";
+        } else if(lastLetter == "o") {
+            return name + "oes";
+        } else {
+            return name + lastLetter + "s"
+        }
     }
 
     let supportedPrimitives = ["ObjectId", "String", "Int", "Int32", "Bool", "Document", "Double", "Data", "Binary", "Date", "RegularExpression"];
     let supportedReturnTypes = ["JSONObject", "JSONArray", "JSON", "Response", "ResponseRepresentable"];
     let models = (types.based["Model"] || []);
+    let exposedMethods = {};
 
     let generatedModels = [];
     let modelIndex = 0;
@@ -49,19 +71,71 @@ extension <%- model.name %> : StringInitializable {
         }
     }<% }); %>
     
-    func integrate(with droplet: Droplet) {
-    <% model.allMethods.forEach(method => {
-        let basicReturnType = supportedReturnTypes.includes(method.unwrappedReturnTypeName);
+    fileprivate static func integrate(with droplet: Droplet, prefixed prefix: String = "/") {<%
 
-        if(!basicReturnType || method.returnType.based.ResponseRepresentable) {
+    let methods = [];
+    model.allMethods.forEach(method => {
+        let basicReturnType = supportedReturnTypes.includes(method.unwrappedReturnTypeName);
+        let permissions = method.annotations["permissions"];
+
+        if((!basicReturnType && !method.returnType.based.ResponseRepresentable && !method.isInitializer) || !permissions) {
             return;
         }
 
-        // TODO: Call method appropriately
-            %>
-    <%});%>
+        // Create/POST
+        if(method.isInitializer) {%>
+        droplet.post("<%-plural(model.name.toLowerCase())%>") { request in
+            return "";
+        }
+        <% } else if(method.isStatic) {
+            // Find/Factory Create (GET, POST)
+        } else {
+            // Update/Delete (PUT, PATCH, DELETE)
+            // Needs an instance
+        }
+
+        methods.push(method);
+    });
+    %>
+    }
+}<% } %>
+
+extension Meow {
+    public static func integrate(with droplet: Droplet) {<%
+        modelIndex = 0;
+        while(modelIndex < generatedModels.length) {
+            let model = generatedModels[modelIndex];
+            modelIndex++; %>
+        <%-model.name%>.integrate(with: droplet)<% } %>
     }
 }
-<%
-    }
+
+<% if(Object.size(exposedMethods) > 0) { %>
+public enum ExposedMethods : String {
+    <%
+        function makeMethodSignature(method) {
+            return method.parameters.map(p => ((p.name || "") + p.unwrappedTypeName)).join("And");
+        }
+
+        function makeExposedMethodName(typeName, method) {
+            return "exposedAPIMethod" + makeMethodSignature(method) + "For" + typeName;
+        }
+
+        let key;
+
+        for(key in exposedMethods) {
+            if(!exposedMethods.hasOwnProperty(key)) {
+                continue;
+            }
+
+            let methodPosition = 0;
+            let methods = exposedMethods[key];
+
+            while(methodPosition < methods.length) {
+                %>case <%-key%> = "<%-makeExposedMethodName(key, methods[methodPosition])%>"
+                <%
+            }
+        }
 %>
+}
+<%}%>
