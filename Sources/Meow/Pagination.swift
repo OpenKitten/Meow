16328becca45081e212fcbf8dc8793a73330d698
Robbert Brandsma <i@robbertbrandsma.nl>
2017-05-10 12:32:29 +0200

    Model.paginatedFind

---
+++ Sources/Meow/Pagination.swift
@@ -0,0 +1,45 @@
+//
+//  Pagination.swift
+//  Tikcit
+//
+//  Created by Robbert Brandsma on 10-05-17.
+//
+//
+
+import Foundation
+
+extension Model {
+    public typealias PaginatedFindResult = (total: Int, perPage: Int, currentPage: Int, lastPage: Int, from: Int, to: Int, data: CollectionSlice<Self>)
+    
+    /// Performs a find on the model, with support for pagination.
+    ///
+    /// - parameter query: The find query
+    /// - parameter sort: Standard MongoKitten sort parameter
+    /// - parameter page: The page number, starting at 1 (thus, specifying a page of 0 is invalid and will throw an error)
+    /// - parameter perPage: The amount of results to include on a page, defaulting to 25
+    public func paginatedFind(_ query: MongoKitten.Query? = nil, sortedBy sort: Sort? = nil, page: Int, perPage: Int = 25) throws -> PaginatedFindResult {
+        let totalCount = try Self.count(query)
+        let skip = (page-1) * perPage
+        let data = try Self.find(query, sortedBy: sort, skipping: skip, limitedTo: perPage, withBatchSize: perPage)
+        
+        var lastPage = totalCount / perPage
+        if totalCount % perPage > 0 {
+            lastPage += 1
+        }
+        
+        var to = skip + perPage
+        if to > totalCount {
+            to = totalCount
+        }
+        
+        return (
+            total: totalCount,
+            perPage: perPage,
+            currentPage: page,
+            lastPage: lastPage,
+            from: skip+1,
+            to: to,
+            data: data
+        )
+    }
+}
