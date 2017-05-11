843779b4110f28b5bb06e6d5115f316c7f4a4d6d
Joannis Orlandos <joannis@orlandos.nl>
2017-05-11 14:09:34 +0200

    More docs

--- Sources/Meow/Serializable.swift
+++ Sources/Meow/Serializable.swift
@@ -1,17 +1,20 @@
 import Foundation
 import BSON
 
+/// Something that can be converted to and from a Primitive
 public protocol Serializable {
     init(restoring source: BSON.Primitive) throws
     
     func serialize() -> BSON.Primitive
 }
 
+/// Something that can be converted to a Document and from a primitive
 public protocol SerializableToDocument : Serializable {
     func serialize() -> BSON.Document
 }
 
 extension SerializableToDocument {
+    /// A helper for the primitive serialization
     public func serialize() -> BSON.Primitive {
         return self.serialize() as Document
     }
