6a05fa4bb1f90996140caa4027e055c204ef5a2c
Robbert Brandsma <i@robbertbrandsma.nl>
2017-05-11 14:51:20 +0200

    Add missing DispatchTime helper

---
+++ Sources/Meow/Helpers.swift
@@ -0,0 +1,26 @@
+//
+//  Helpers.swift
+//  Tikcit
+//
+//  Created by Robbert Brandsma on 11-05-17.
+//
+//
+
+import Foundation
+import Dispatch
+
+// (parts of) this file are from https://github.com/vapor/core
+
+extension Double {
+    fileprivate var nanoseconds: UInt64 {
+        return UInt64(self * Double(1_000_000_000))
+    }
+}
+
+
+extension DispatchTime {
+    internal init(secondsFromNow: Double) {
+        let uptime = DispatchTime.now().rawValue + secondsFromNow.nanoseconds
+        self.init(uptimeNanoseconds: uptime)
+    }
+}
