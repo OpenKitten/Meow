3f6a59a6c9b737bacc911791eb5584a222f3fd46
Joannis Orlandos <joannis@orlandos.nl>
2017-05-12 15:06:37 +0200

    Added extra serialisable docs

--- Sources/Meow/Serializable.swift
+++ Sources/Meow/Serializable.swift
@@ -4,6 +4,8 @@ import BSON
 /// Something that can be converted to and from a BSON Primitive
 public protocol Serializable {
     /// Initialize an instance of the Serializable, restoring from the BSON Primitive format
+    ///
+    /// This initializer *must* only be called from within Meow. Never call this method fromy our application.
     init(restoring source: BSON.Primitive) throws
     
     /// Serialize the Serializable into its BSON Primitive format
