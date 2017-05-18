890a0509706a7445ebcbe85c8a6fa4af156a64c1
Robbert Brandsma <i@robbertbrandsma.nl>
2017-05-18 11:34:00 +0200

    Removed MeowSample

--- Package.swift
+++ Package.swift
@@ -3,8 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Meow",
     targets: [
-        Target(name: "Meow"),
-        Target(name: "MeowSample", dependencies: ["Meow"])
+        Target(name: "Meow")
     ],
     dependencies: [
        .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 4)
