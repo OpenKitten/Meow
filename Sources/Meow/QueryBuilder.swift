c0bf8e940d7797b42e4a7dadc14ba0599e462eee
Robbert Brandsma <i@robbertbrandsma.nl>
2017-06-26 21:37:55 +0200

    Typesafe query proof of concept

--- Sources/Meow/QueryBuilder.swift
+++ Sources/Meow/QueryBuilder.swift
@@ -8,6 +8,33 @@
 import Foundation
 import MongoKitten
 
+public protocol KeyPathListable {
+    static var allKeyPaths: [String : AnyKeyPath] { get }
+}
+
 public func ==(lhs: String, rhs: Referencing) -> Query {
     return lhs == rhs.reference
+}
+
+public struct TypesafeQuery<Root> {
+    var query: Query
+}
+
+public enum QueryBuilderError : Error {
+    case unknownKeyPath
+}
+
+public func ==<Root, Value>(lhs: KeyPath<Root, Value>, rhs: Value) throws -> TypesafeQuery<Root> where Value : Primitive, Root : KeyPathListable {
+    // Find the key path
+    guard let path = (Root.allKeyPaths.first { $0.value == lhs }) else {
+        throw QueryBuilderError.unknownKeyPath
+    }
+    
+    return TypesafeQuery(query: path.key == rhs)
+}
+
+public extension _Model {
+    public static func find(_ query: TypesafeQuery<Self>) throws -> AnySequence<Self> {
+        return try Self.find(query.query)
+    }
 }
