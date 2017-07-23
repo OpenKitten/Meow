bac8ec0dd29d2a99e9e36986db0a34df7afbbd3b
Joannis Orlandos <joannis@orlandos.nl>
2017-07-23 23:38:16 +0200

    Exposed a product

--- Package.swift
+++ Package.swift
@@ -6,6 +6,12 @@ import PackageDescription
 
 let package = Package(
     name: "Meow",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Meow",
+            targets: ["Meow"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", .revision("async")),
         
